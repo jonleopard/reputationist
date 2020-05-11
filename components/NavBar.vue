95a2588d8dd312fbf74ce83d06ec5d4cda2b69e3
Jon Leopard <jontron2000@gmail.com>
2020-05-11 14:31:38 +0200

    First

---
+++ components/NavBar.vue
@@ -0,0 +1,86 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img
+          src="https://bulma.io/images/bulma-logo.png"
+          width="112"
+          height="28"
+        />
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Services
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              Reputation Management
+            </a>
+            <a class="navbar-item">
+              Enterprise SEO
+            </a>
+          </div>
+        </div>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Who we help
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              C-Suites & Executives
+            </a>
+            <a class="navbar-item">
+              Enterprise SEO
+            </a>
+          </div>
+        </div>
+
+        <a class="navbar-item">
+          Documentation
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav></template
+>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
