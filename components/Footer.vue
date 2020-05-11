381edacc4aee4b232ec9f34d9c11141a36b66c79
Jon Leopard <jontron2000@gmail.com>
2020-05-11 19:33:22 +0200

    Working on footer

---
+++ components/Footer.vue
@@ -0,0 +1,125 @@
+<template>
+  <footer class="footer has-text-white level has-background-grey-dark columns">
+    <div class="column">
+      <img src="/logo-main-white.png" width="250px" />
+      <div class="content">
+        <p>
+          We are on a mission to protect your online reputation and restore the
+          trust of your customers and clients. We suppress damaging search
+          results and help you actively manage your online standing.
+        </p>
+        <p class="is-size-7">
+          <a href="#" class="has-text-primary">Terms & Conditions</a>
+          <a href="#" class="has-text-primary">Privacy Policy</a>
+        </p>
+        <p class="is-size-7">
+          © Copyright 2020 • reputation.ist • All rights reserved
+        </p>
+      </div>
+    </div>
+
+    <div class="column">
+      <div>
+        <h1 class="title has-text-white">Navigate</h1>
+        <ul>
+          <li>
+            <a href="#" class="has-text-primary">Reputation Management</a>
+          </li>
+          <li>
+            <a href="#" class="has-text-primary">Enterprise SEO</a>
+          </li>
+          <li>
+            <a href="#" class="has-text-primary">C-Suite & Executives</a>
+          </li>
+          <li>
+            <a href="#" class="has-text-primary">Individuals</a>
+          </li>
+
+          <li>
+            <a href="#" class="has-text-primary">Small Businesses</a>
+          </li>
+
+          <li>
+            <a href="#" class="has-text-primary">Large Companies</a>
+          </li>
+
+          <li>
+            <a href="#" class="has-text-primary">Enterprise Companies</a>
+          </li>
+
+          <li>
+            <a href="#" class="has-text-primary">About Us</a>
+          </li>
+
+          <li>
+            <a href="#" class="has-text-primary">Case Studies</a>
+          </li>
+
+          <li>
+            <a href="#" class="has-text-primary">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="column">
+      <div>
+        <h1 class="title has-text-white">Contact Us</h1>
+        <ul>
+          <li>
+            <a
+              href="tel:+1 888 979 6878"
+              class="has-text-primary"
+              target="_self"
+              >tel:+1 888 979 6878</a
+            >
+          </li>
+          <li>
+            <a href="mailto:hello@reputation.ist" class="has-text-primary"
+              >hello@reputation.ist</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div>
+        <span class="icon has-text-info">
+          <i class="fa"></i>
+        </span>
+      </div>
+      <div>
+        <h1 class="title is-5 has-text-white">Subscribe to our Newsletter</h1>
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <div class="control">
+                <input class="input" type="text" placeholder="First Name" />
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <input class="input" type="text" placeholder="Last Name" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <input class="input" type="email" placeholder="Email address" />
+          </div>
+        </div>
+        <div class="field">
+          <div class="control">
+            <button class="button is-primary">
+              Subscribe
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
