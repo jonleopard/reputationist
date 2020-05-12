a2d038eb6961b42201fc52f87012aa72d078a21c
Jon Leopard <jontron2000@gmail.com>
2020-05-12 18:50:40 +0200

    Almost finished with footer. Add netlify form later

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,25 +1,25 @@
 <template>
   <footer class="footer has-text-white level has-background-grey-dark columns">
-    <div class="column">
-      <img src="/logo-main-white.png" width="250px" />
-      <div class="content">
-        <p>
-          We are on a mission to protect your online reputation and restore the
-          trust of your customers and clients. We suppress damaging search
-          results and help you actively manage your online standing.
-        </p>
-        <p class="is-size-7">
-          <a href="#" class="has-text-primary">Terms & Conditions</a>
-          <a href="#" class="has-text-primary">Privacy Policy</a>
-        </p>
-        <p class="is-size-7">
-          © Copyright 2020 • reputation.ist • All rights reserved
-        </p>
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <img src="/logo-main-white.png" width="250px" />
+        <div class="content">
+          <p>
+            We are on a mission to protect your online reputation and restore
+            the trust of your customers and clients. We suppress damaging search
+            results and help you actively manage your online standing.
+          </p>
+          <p class="is-size-7">
+            <a href="#" class="has-text-primary">Terms & Conditions</a>
+            <a href="#" class="has-text-primary">Privacy Policy</a>
+          </p>
+          <p class="is-size-7">
+            © Copyright 2020 • reputation.ist • All rights reserved
+          </p>
+        </div>
       </div>
-    </div>
 
-    <div class="column">
-      <div>
+      <div class="column">
         <h1 class="title has-text-white">Navigate</h1>
         <ul>
           <li>
@@ -60,59 +60,79 @@
           </li>
         </ul>
       </div>
-    </div>
 
-    <div class="column">
-      <div>
+      <div class="column">
         <h1 class="title has-text-white">Contact Us</h1>
-        <ul>
-          <li>
-            <a
-              href="tel:+1 888 979 6878"
-              class="has-text-primary"
-              target="_self"
-              >tel:+1 888 979 6878</a
-            >
-          </li>
-          <li>
-            <a href="mailto:hello@reputation.ist" class="has-text-primary"
-              >hello@reputation.ist</a
-            >
-          </li>
-        </ul>
-      </div>
-      <div>
-        <span class="icon has-text-info">
-          <i class="fa"></i>
-        </span>
-      </div>
-      <div>
-        <h1 class="title is-5 has-text-white">Subscribe to our Newsletter</h1>
-        <div class="field is-horizontal">
-          <div class="field-body">
-            <div class="field">
-              <div class="control">
-                <input class="input" type="text" placeholder="First Name" />
+        <div class="columns">
+          <div class="column">
+            <ul>
+              <li>
+                <a
+                  href="tel:+1 888 979 6878"
+                  class="has-text-primary"
+                  target="_self"
+                  >tel:+1 888 979 6878</a
+                >
+              </li>
+              <li>
+                <a href="mailto:hello@reputation.ist" class="has-text-primary"
+                  >hello@reputation.ist</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="columns">
+          <a
+            href="https://www.facebook.com/reputationist.agency"
+            class="column"
+          >
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'facebook' }"
+              style="color: #fff; font-size: 32px;"
+            />
+          </a>
+          <a href="https://www.instagram.com/reputation.ist/" class="column">
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'instagram' }"
+              style="color: #fff; font-size: 32px;"
+            />
+          </a>
+          <a href="https://twitter.com/Reputation_ist" class="column">
+            <font-awesome-icon
+              :icon="{ prefix: 'fab', iconName: 'twitter' }"
+              style="color: #fff; font-size: 32px;"
+            />
+          </a>
+        </div>
+        <div>
+          <h1 class="title is-5 has-text-white">Subscribe to our Newsletter</h1>
+          <div class="field is-horizontal">
+            <div class="field-body">
+              <div class="field">
+                <div class="control">
+                  <input class="input" type="text" placeholder="First Name" />
+                </div>
               </div>
-            </div>
-            <div class="field">
-              <div class="control">
-                <input class="input" type="text" placeholder="Last Name" />
+              <div class="field">
+                <div class="control">
+                  <input class="input" type="text" placeholder="Last Name" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="field">
-          <div class="control">
-            <input class="input" type="email" placeholder="Email address" />
+          <div class="field">
+            <div class="control">
+              <input class="input" type="email" placeholder="Email address" />
+            </div>
           </div>
-        </div>
-        <div class="field">
-          <div class="control">
-            <button class="button is-primary">
-              Subscribe
-            </button>
+          <div class="field">
+            <div class="control">
+              <button class="button is-primary">
+                Subscribe
+              </button>
+            </div>
           </div>
         </div>
       </div>
